c07c41e125614353a69181137966b9ff55d201e0
Jyoutis <159123869+Jyotiux@users.noreply.github.com>
2025-09-04 02:15:21 +0530

    Update firebaseAdmin.js

--- server/firebaseAdmin.js
+++ server/firebaseAdmin.js
@@ -1,10 +1,22 @@
-// backend/firebaseAdmin.js
-import admin from "firebase-admin";
-import fs from "fs";
+// // backend/firebaseAdmin.js
+// import admin from "firebase-admin";
+// import fs from "fs";
 
-const serviceAccount = JSON.parse(
-  fs.readFileSync(new URL("./serviceAccountKey.json", import.meta.url))
-);
+// const serviceAccount = JSON.parse(
+//   fs.readFileSync(new URL("./serviceAccountKey.json", import.meta.url))
+// );
+
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount),
+// });
+
+// export default admin;
+
+
+import admin from "firebase-admin";
+
+// Load from environment variable
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
